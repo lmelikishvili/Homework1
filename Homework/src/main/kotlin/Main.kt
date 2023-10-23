c4bfa77ce53d9393e8790c61bb7639296be9501d
lmelikishvili <levanimelikishvili@gmail.com>
2023-10-23 22:28:35 +0400

    final

--- Homework/src/main/kotlin/Main.kt
+++ Homework/src/main/kotlin/Main.kt
@@ -1,7 +1,50 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var isActive = true
+    var x = ""
+    var y = ""
+    var z = 0
+    var answer = ""
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    do {
+
+        println("შეიყვანეთ X ცვლადის მნიშვნელობა")
+        x = readln()
+        do {
+            if (!isValidInt(x)){
+                println("შეიყვანეთ X ცვლადის მნიშვნელობა (ციფრი)")
+                x = readln()
+            }
+        }while (!isValidInt(x))
+
+        println("შეიყვანეთ Y ცვლადის მნიშვნელობა")
+        y = readln()
+        do {
+            if (!isValidInt(y)){
+                println("შეიყვანეთ Y ცვლადის მნიშვნელობა (ციფრი)")
+                y = readln()
+            }
+        }while (!isValidInt(y))
+
+        z = x.toInt() + y.toInt()
+        println("X და Y ჯამი არის: $z")
+
+        do {
+            println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>?")
+            answer = readln()
+            if (answer == "არა"){
+                isActive = false
+                break
+            }
+            if (answer != "დიახ"){
+                println("გთხოვთ ჩაწეროთ სწორად!!!")
+            }
+
+        }while (answer != "დიახ")
+
+    } while (isActive)
+}
+
+fun isValidInt(number: String): Boolean{
+    return number.toIntOrNull() != null
 }
